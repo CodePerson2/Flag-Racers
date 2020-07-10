f813581208da1613123eb5808b8a57c31637acfb
CodePerson2 <mattiasstroman@gmail.com>
2020-07-10 00:18:26 -0700

    adding new database

---
+++ public/racetrack.js
@@ -0,0 +1,21 @@
+/*
+  Sends user to 'race finished' page
+*/
+function endGame() {
+    location.href= "https://racetrackgp.herokuapp.com/endPage.html";
+  }
+  
+  /*
+    Sends user to racetrack page
+  */
+  function startGame() {
+    location.href= "https://racetrackgp.herokuapp.com/racetrack.html";
+  }
+  
+  /*
+    Sends user to flag selection page
+    (empty atm)
+  */
+  function selectFlag() {
+  
+  }
