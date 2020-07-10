eaf753a87e603b232ccf9df1fbcccd8d8ff76256
CodePerson2 <mattiasstroman@gmail.com>
2020-07-09 23:45:44 -0700

    adding new database

--- public/login.js
+++ public/login.js
@@ -39,10 +39,13 @@ function send(type) {
             var res = JSON.parse(this.responseText);
             console.log(res);
             if(res){alert("Incorrect username or password");}
-            else{
+            else if(!res){
                 var url = "flag.html?" + name;
                 window.location.replace(url);
 
+            }
+            else{
+                console.log(res);
             }
           }
             
