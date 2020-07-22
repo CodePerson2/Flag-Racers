96138256f564877221199059c25798573e6cbaec
CodePerson2 <mattiasstroman@gmail.com>
2020-07-21 17:17:59 -0700

    new page chatbox

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ var login = express()
         res.send({res : 1, data : error});
       }
       else{
-        if(result.length > 0){
+        if(result.rows.length > 0){
           res.send({res : 0, data : 'success'});
         }
         else{
