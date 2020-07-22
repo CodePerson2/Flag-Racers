3be077751215a1d7c4cfee63fc1adbb6ae76b279
CodePerson2 <mattiasstroman@gmail.com>
2020-07-22 00:38:32 -0700

    new page chatbox

--- index.js
+++ index.js
@@ -162,7 +162,7 @@ var login = express()
     })
   });
 
-  login.get('/getmess/:val', (req, res) => {
+  login.post('/getmess/:val', (req, res) => {
     var val = req.params.val;
     var val = JSON.parse(val);
 
