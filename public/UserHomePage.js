ee4095841a1a1b92b4501d2151f5c245ce07060c
trinception <trinitye@sfu.ca>
2020-07-22 21:57:46 -0700

    User Home Page js update

--- public/UserHomePage.js
+++ public/UserHomePage.js
@@ -5,18 +5,16 @@ function race() {
   const  queryString = window.location.search;
 
   const urlParams = new URLSearchParams(queryString);
-  //const name = urlParams.get('name');
-  console.log(name);
+
   window.location = "flag.html?" + urlParams;
   //location.href="flag.html";
 }
 
 function chat() {
-  /*const  queryString = window.location.search;
+  const  queryString = window.location.search;
 
   const urlParams = new URLSearchParams(queryString);
-  const name = urlParams.get('name');
-  alert(name);
-  window.location = "Chatbox.html?" + name;*/
-  location.href="Chatbox.html"
+
+  window.location = "Chatbox.html?" + urlParams;
+  //location.href="Chatbox.html"
 }
