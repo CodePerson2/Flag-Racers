1b7f211f7ef04c4f50a38690f644857446745348
CodePerson2 <mattiasstroman@gmail.com>
2020-07-23 13:32:43 -0700

    messaging update

--- public/Chatbox-conn.js
+++ public/Chatbox-conn.js
@@ -130,7 +130,7 @@ function getchat(chatid, num, messid, userid){
             var res = JSON.parse(this.responseText);
             console.log(res);
             for(var i = res.data.length; i > 0; i--){
-                console.log(this.responseText.data);
+                console.log(res.data);
                 console.log(res.data.userid);
                 if(res.data[i].userid == userid){
                     message(res.data[i].message);
