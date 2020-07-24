3c9759652448d98500ef0143bdd80f1c2449126e
trinception <trinitye@sfu.ca>
2020-07-24 02:02:00 -0700

    FIXING ISSUE PLEASE NOTE THIS IS MATERIAL FROM BEFORE MIDNIGHT

--- public/script.js
+++ public/script.js
@@ -207,11 +207,13 @@ $(function() {
 
     // Temporarily Game restarts upon hitting the End Button
     restart_btn.click(function() {
-          stop_the_game();
+        //location.reload();
+        //stop_the_game();
     });
 
     // Function to stop game when needed
     function stop_the_game() {
+        game_redirect();
         game_over = true;
         cancelAnimationFrame(anim_id);
         cancelAnimationFrame(move_right);
@@ -221,6 +223,16 @@ $(function() {
         restart_div.slideDown();
         restart_btn.focus();
         setHighScore();
+    }
+
+    //End game redirect
+    function game_redirect(){
+      console.log("redirection attempted!");
+      const  queryString = window.location.search;
+
+      const urlParams = new URLSearchParams(queryString);
+
+      window.location = "endPage.html?" + urlParams;
     }
 
     // Collision check with objects
