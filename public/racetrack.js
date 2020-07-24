104dd23231d9aa0fd5c89c1690c0b71141eb6046
trinception <trinitye@sfu.ca>
2020-07-23 20:16:15 -0700

    End page design tweaks

--- public/racetrack.js
+++ public/racetrack.js
@@ -18,3 +18,8 @@ function endGame() {
   function selectFlag() {
     location.href= "flag.html"
   }
+
+  function logout() {
+    document.cookie = "user credentials=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    window.location.replace("Login.html");
+  }
