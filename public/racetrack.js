6dad597073d05d87459d5a2fb2600b1e2ad46668
trinception <trinitye@sfu.ca>
2020-07-23 20:22:16 -0700

    Small tweaks

--- public/racetrack.js
+++ public/racetrack.js
@@ -19,6 +19,9 @@ function endGame() {
     location.href= "flag.html"
   }
 
+/*
+  Logs user out
+*/
   function logout() {
     document.cookie = "user credentials=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     window.location.replace("Login.html");
