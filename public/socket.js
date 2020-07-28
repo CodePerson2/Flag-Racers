b1129d10c22b3035f6b1f74c1118b8175cc091f9
CodePerson2 <mattiasstroman@gmail.com>
2020-07-27 21:14:33 -0700

    socket

--- public/socket.js
+++ public/socket.js
@@ -1,13 +1,2 @@
 var socket = io.connect('/');
-socket.on('speed', function (data) {
-    console.log('speed Message Received!');
-    console.log(data);    
-});
-
-socket.on('beep', function (data) {
-    console.log('beep Message Received!');    
-    console.log(data);    
-});
-socket.emit("beep", {beep : true});
-socket.emit("change-speed", {"change-speed" : true});
-socket.emit("ios-connection", {"ios-connection" : true});
+socket.on('message', function(data){document.write(data)});
