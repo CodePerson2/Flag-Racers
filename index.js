a008ac96642da0fdb27ec3af01f9fd23038c04bc
CodePerson2 <mattiasstroman@gmail.com>
2020-07-27 21:37:37 -0700

    socket

--- index.js
+++ index.js
@@ -214,9 +214,6 @@ var login = express()
     console.log('A user connected');
  
     //Send a message after a timeout of 4seconds
-    setTimeout(function() {
-       socket.send('Sent a message 4seconds after connection!');
-    }, 4000);
     socket.emit('num', {description: io.sockets.clients()});
  
     socket.on('disconnect', function () {
